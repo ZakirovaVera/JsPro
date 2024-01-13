08a1dbc951f47bc2262f688b0006bca87757b7dd
ZakirovaVera <zakirovavk@gmail.com>
2024-01-13 20:56:08 +0500

    ветка test итог

--- dz3/test.js
+++ dz3/test.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // const initialJSON = `[
-    //     {"product":"Apple iPhone 13","reviews":[{"id":"1","text":"текст отзыва"}]}]`;
-    const initialJSON = `[{"product":""}]`;
+    const initialJSON = `[
+        {"product":"Apple iPhone 13", "id": 1,"reviews":[{"id":"1","text":"отличный"}]}]`;
 
     const keyStorageProductReviews = "productReviews";
     let initialData = localStorage.getItem(keyStorageProductReviews);
@@ -15,64 +14,80 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const reviewHtml = reviews.map((review) => getReviewHtml(review)).join('');
 
-    
+
 
     const addBtnElement = document.querySelector('.add-btn');
     const productnameInput = document.querySelector('.productname-input');
     const textInput = document.querySelector('.text-input');
 
     // при клике сохраняются в localStorage введенные данные.
-
     if (addBtnElement) {
         addBtnElement.addEventListener("click", function () {
 
             const productName = productnameInput.value;
             const textrev = textInput.value;
 
-            // const review = {
-            //     product: productName,
-            //     rewiews: [
-            //         {
-            //             id: Date.now(),
-            //             text: textrev,
-            //         }
-            //     ]
-            // };
-            
-            const itemProduct = reviews.find((productName) => reviews.product === productName);
             const review = {
+                product: productName,
+                id: Date.now(),
+                reviews: [
+                    {
                         id: Date.now(),
                         text: textrev,
+                    }
+                ]
             };
-            itemProduct.push(review);
-    
-            // reviews.push(review);
-            localStorage.setItem(keyStorageProductReviews, JSON.stringify(itemProduct));
 
-            // conteinerElement.insertAdjacentHTML('beforeend', getReviewHtml(review));
+            reviews.push(review);
+            localStorage.setItem(keyStorageProductReviews, JSON.stringify(reviews));
+
         })
     }
 
-    const reviewContainerElement = document.querySelector('.review-container');
+    const conteinerProductElement = document.querySelector(".container-product")
     const loadBtnElement = document.querySelector('.load-button');
 
-    if(loadBtnElement){
+    if (loadBtnElement) {
         loadBtnElement.addEventListener('click', function () {
-            
+            conteinerProductElement.innerHTML = reviewHtml;
         });
     }
-// const conteinerElement = document.querySelector(".container")
-
-    // conteinerElement.innerHTML = reviewHtml;
 
 
+    const btnProductElement = document.querySelector('.btn-product');
+
+    conteinerProductElement.addEventListener("click", function (event) {
+
+        if (event.target.classList.contains("btn-product")) {
+            const containerProduct = event.target.parentNode;
+            const el = containerProduct.querySelector("#welcomeDiv");
+            el?.classList.toggle('hidden');
+            return;
+        }
+
+        if (event.target.classList.contains("product-review__delete")) {
+            const reWrappElement = event.target.closest('.product');
+            const productId = event.target.getAttribute("product-id");
+
+            const indexProduct = reviews.findIndex(x => x.id == productId);
+            reviews.splice(indexProduct, 1);
+            localStorage.setItem(keyStorageProductReviews, JSON.stringify(reviews));
+
+            reWrappElement.remove();
+            return;
+        }
+    }
+    )
 
     function getReviewHtml(rev) {
+        let id = rev.reviews?.map((item) => item.id);
+        let text = rev.reviews?.map((item) => item.text);
+
         return `<div class="product">
-        <button class="click-product">${rev.product}</button>
-        <div class="revWrapp">
-            <p class="product-review" data-id="${rev.reviews.id}>${rev.reviews.text}</p>
-            <button class="product-review__delete">Delete</button>
+        <button class="btn-product">${rev.product}</button>
+        <div class="revWrapp hidden" id="welcomeDiv" >
+            <p class="product-review" data-id="${id}">${text}</p>
+            <button class="product-review__delete" product-id="${rev.id}" review-id="${id}">Delete</button>
         </div>
     </div>`;
     }
